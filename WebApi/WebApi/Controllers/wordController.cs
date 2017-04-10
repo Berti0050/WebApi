003d2b8fec5272b5550e5cb3925e46796eb07130
Berti0050 <bertialessander@gmail.com>
2017-04-10 17:49:32 -0400

    V1

---
+++ WebApi/WebApi/Controllers/wordController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using WebApi.Models;
+using System.Web;
+
+
+namespace WebApi.Controllers
+{
+    public class wordController : ApiController
+    {
+        // POST: api/word
+        public string Post([FromBody]dataIn data)
+        {
+            dataRes respuesta;
+            try{
+                respuesta = new dataRes();
+                if (data.data.Length == 4)
+                {
+                    respuesta.data = data.data.ToUpper();
+                    respuesta.code = 200;
+                    respuesta.description = "OK";
+                }
+                else
+                {
+                    respuesta.code = 400;
+                    respuesta.description = "Bad Request";
+                }
+            }catch(Exception e)
+            {
+                respuesta = new dataRes();
+                respuesta.data = e.Message;
+                respuesta.code = 500;
+                respuesta.description = "Internal Server Error";
+
+                var jsonRespuestaE = new JavaScriptSerializer().Serialize(respuesta);
+
+                return jsonRespuestaE;
+            }
+            
+            var jsonRespuesta = new JavaScriptSerializer().Serialize(respuesta);
+
+            return jsonRespuesta;
+        }
+    }
+}
